80586b05399ed56a0e8ee1cd6308a43dfc4a206b
esciafardini <edward.ciafardini@aclaimant.com>
2023-05-15 10:25:32 -0400

    Progress on countries exercise

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,32 +1,60 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const CountryNames = ({ names }) => {
-  let nameDisplays = names.map(item => <div> {item} </div>)
-  return <div>{nameDisplays}</div>
+const CountryDisplay = ({ country }) => {
+  return <>
+    <h1>{country.name.common}</h1>
+    <div>capital: {country.capital[0]}</div>
+    <div>area: {country.area}</div>
+    <p>languages:</p>
+    <ul>
+      {Object.values(country.languages).map((obj) => <li>{obj}</li>)}
+    </ul>
+    <img src={country.flags['png']} alt="FLAG" />
+  </>
+}
+
+const CountryListItem = ({ names, clickHandler }) => {
+  let nameDisplays = names.map(item => <div>
+    {item.name.common}
+    <button onClick={() => clickHandler(item.name.common)}>show</button>
+  </div>
+  )
+  return <>
+    <div>{nameDisplays}</div>
+  </>
 }
 
 function App() {
 
   const [search, setSearch] = useState('')
+  const [show, setShow] = useState(false)
   const [countries, setCountries] = useState([])
   const [filteredCountries, setFilteredCountries] = useState([])
 
+  const clickShow = (countryName) => {
+    setSearch(countryName)
+    setShow(true)
+  }
+
   useEffect(() => {
     axios.get('https://restcountries.com/v3.1/all')
-      .then((res) => setCountries(res.data.map((countryInfo) => countryInfo.name.common)))
+      .then((res) => setCountries(res.data))
   }, [])
 
   useEffect(() => {
-    setFilteredCountries(countries.filter((countryName) => countryName.toLowerCase().includes(search.toLowerCase())))
+    setFilteredCountries(countries.filter((countryInfo) => countryInfo.name.common.toLowerCase().includes(search.toLowerCase())))
   }, [search])
 
   return (
     <>
-      <input value={search} onChange={(e) => setSearch(e.target.value)} />
+      <input value={search} onChange={(e) => {
+        setShow(false)
+        setSearch(e.target.value)
+      }} />
       {(filteredCountries.length > 10) && <div>Too many matches, specify another filter</div>}
-      {(filteredCountries.length <= 10 && filteredCountries.length !== 1) && <CountryNames names={filteredCountries} />}
-      {(filteredCountries.length === 1) && <div>a beautiful display</div>}
+      {(filteredCountries.length <= 10) && <CountryListItem clickHandler={clickShow} names={filteredCountries} />}
+      {show && <CountryDisplay country={filteredCountries[0]} />}
     </>
   );
 }
